327aa9ee05464a449075ce4d582ec44376fbcf77
Moonike <2462599013@qq.com>
2025-02-18 17:35:57 +0800

    feat(shortlink): 开发获取网页标题功能
    - 在 project 和 admin模块中添加获取网页标题的相关接口和实现
    - 新增 UrlTitleController 和 UrlTitleService 用于处理网页标题请求
    - 使用 Jsoup 库解析 HTML 文档，获取网页标题

--- admin/src/main/java/com/moonike/admin/remote/ShortLinkRemoteService.java
+++ admin/src/main/java/com/moonike/admin/remote/ShortLinkRemoteService.java
@@ -59,7 +59,21 @@ public interface ShortLinkRemoteService {
         return JSON.parseObject(resultPageStr, new TypeReference<>() {});
     }
 
+    /**
+     * 远程调用更新短链接
+     * @param requestParam
+     */
     default void updateShortLink(ShortLinkUpdateReqDTO requestParam) {
         HttpUtil.post("http://127.0.0.1:8001/api/shortlink/v1/update", JSON.toJSONString(requestParam));
+    }
+
+    /**
+     * 根据 URL 获取标题
+     * @param url 目标网站地址
+     * @return 网站标题
+     */
+    default Result<String> getTitleByUrl(String url) {
+        String resultStr = HttpUtil.get("http://127.0.0.1:8001/api/short-link/v1/title?url=" + url);
+        return JSON.parseObject(resultStr, new TypeReference<>() {});
     }
 }
